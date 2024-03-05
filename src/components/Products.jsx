050a19b2e93ea038bd4b169eb2a7c7d10f56c9ef
AnanaszosJoe <ananaszosjoe@gmail.com>
2024-03-05 18:08:33 +0100

    first commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { products } from '../data'
+import { SingleProduct } from './SingleProduct';
+
+export const Products = () => {
+  //console.log(products);
+  return (
+    <div className="container shadow ">
+      <div className='d-flex justify-content-center gap-3 flex-wrap bg-light'>
+        <h3 className='w-100 m-2 text-primary '>Our Products</h3>
+        {products.map(obj=><SingleProduct key={obj.id} {...obj}/>)}
+      </div>
+    </div>
+  )
+}
+
